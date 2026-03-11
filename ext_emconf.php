9225ea840db13877be9f7154c8058019bab1e7b2
Thomas Rawiel <thomasrawiel@users.noreply.github.com>
2026-03-11 10:58:30 +0100

    [feat] prepare v14

--- ext_emconf.php
+++ ext_emconf.php
@@ -8,10 +8,10 @@ $EM_CONF[$_EXTKEY] = [
     'author' => 'Thomas Rawiel',
     'author_email' => 'thomas.rawiel@gmail.com',
     'clearCacheOnLoad' => 0,
-    'version' => '1.0.4',
+    'version' => '1.1.0',
     'constraints' => [
         'depends' => [
-            'typo3' => '12.4.0-13.4.99',
+            'typo3' => '12.4.0-14.99.99',
         ],
         'conflicts' => [],
         'suggests' => [],
